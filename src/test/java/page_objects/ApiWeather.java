be8704d7edda1d2beff85f4ef1d9e4db697b73a4
jcsmoreira <joao.moreira@ostmodern.co.uk>
2019-03-13 16:13:09 +0000

    Added String to Double convertion to avoid possible problems with results format

--- src/test/java/page_objects/ApiWeather.java
+++ src/test/java/page_objects/ApiWeather.java
@@ -55,8 +55,4 @@ public class ApiWeather {
         assertThat(tempMax, is(greaterThanOrEqualTo(tempMin)));
     }
 
-    public static boolean isNumeric(String str) {
-        return str.matches("-?\\d+(\\.\\d+)?");  //match a number with optional '-' and decimal.
-    }
-
 }
